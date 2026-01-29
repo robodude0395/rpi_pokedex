1f9e7731c6c0c52b30836fce1cdbbf81e30d391b
Omar <robodude20000@gmail.com>
2026-01-29 23:34:42 +0000

    Packages display_and_input as an installable package

--- display_and_input/setup.py
+++ display_and_input/setup.py
@@ -1,11 +1,11 @@
-from setuptools import setup, find_packages
+from setuptools import setup
 
 setup(
     name='display_and_input',
     version='0.1.0',
     description='ST7789 display and input utilities for Raspberry Pi',
     author='Omar Da Best',
-    packages=find_packages(),
+    packages=['.'],
     include_package_data=True,
     install_requires=[
         'spidev',
