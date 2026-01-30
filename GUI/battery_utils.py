77a95aae2f8fa68000688b0422c76894b2f43571
Omar <robodude20000@gmail.com>
2026-01-30 17:06:30 +0000

    Begun work on the main interface

--- GUI/battery_utils.py
+++ GUI/battery_utils.py
@@ -8,8 +8,9 @@ class BatteryReader:
         self.min_voltage = min_voltage
         self.max_voltage = max_voltage
         self.channel = channel
-        self.i2c = board.I2C()
-        self.ads = ADS1015(self.i2c)
+        self.i2c = busio.I2C(board.SCL, board.SDA)
+        self.ads = ADS.ADS1115(self.i2c)
+        self.ads.gain = 1
         self.chan = AnalogIn(self.ads, self.channel)
 
     def get_percent(self):
